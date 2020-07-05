22e2235b101943682b58f9269bb4e89df69f9aaf
atat634 <lgg4.at.mss@gmail.com>
2020-07-05 22:02:19 +0900

    add box sphere

---
+++ src/work.js
@@ -0,0 +1,11 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+import { Box } from './boxGeometry';
+import { Sphere } from './sphereGeometry';
+
+export const Work = () => (
+    <div>
+        <Box />
+        <Sphere />
+    </div>
+);
